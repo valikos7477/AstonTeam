6511513cd870f27c91ff70f87dd2a29b57aa07eb
baiv84 <65267292+baiv84@users.noreply.github.com>
2025-02-07 23:38:54 +0700

    Добавил CustomArrayList класс

--- src/main/java/com/aston/MainApp.java
+++ src/main/java/com/aston/MainApp.java
@@ -2,8 +2,10 @@ package com.aston;
 
 import java.util.Arrays;
 import java.util.List;
-import com.aston.utils.SortingEngine;
 
+import com.aston.entities.Sortable;
+import com.aston.utils.SortingEngine;
+import com.aston.tools.CustomArrayList;
 
 public class MainApp {
     public static void main(String[] args) {
@@ -12,5 +14,9 @@ public class MainApp {
         SortingEngine.insertSorting(array);
         List<Integer> arrayAsList = Arrays.asList(array);
         arrayAsList.stream().forEach(System.out::println);
+
+        List<Sortable> list = new CustomArrayList<>();
+
+
     }
 }
---
+++ src/main/java/com/aston/tools/CustomArrayList.java
@@ -0,0 +1,64 @@
+package com.aston.tools;
+
+
+import java.util.AbstractList;
+import java.util.Arrays;
+
+public class CustomArrayList<E> extends AbstractList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public CustomArrayList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public CustomArrayList(int capacity) {
+        elements = new Object[capacity];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public E get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        }
+        return (E) elements[index];
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        }
+        ensureCapacity();
+        for (int i = size - 1; i >= index; i--) {
+            elements[i + 1] = elements[i];
+        }
+        elements[index] = element;
+        size++;
+    }
+
+    @Override
+    public E remove(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        }
+        Object item = elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        return (E) item;
+    }
+
+    private void ensureCapacity() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+}
