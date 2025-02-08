a093129f26b796577138c0d41ab39da3124e3195
baiv84 <65267292+baiv84@users.noreply.github.com>
2025-02-08 20:33:41 +0700

    Взял реализацию CustomArrayList от DennisFeature

--- src/main/java/com/aston/MainApp.java
+++ src/main/java/com/aston/MainApp.java
@@ -1,36 +1,37 @@
 package com.aston;
 
 import com.aston.entities.*;
+import com.aston.tools.CustomArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("*****************");
+        CustomArrayList<Sortable> list = new CustomArrayList<>();
+
         AnimalBuilder animalBuilder = new AnimalBuilder();
         for (int i = 0; i < 100; i++) {
             Animal animal = animalBuilder.randomBuild();
-            System.out.println(animal);
+            list.add(animal);
         }
 
-        System.out.println("*****************");
         BoxBuilder boxBuilder = new BoxBuilder();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             Box box = boxBuilder.randomBuild();
-            System.out.println(box);
+            list.add(box);
         }
 
-        System.out.println("*****************");
         HumanBuilder humanBuilder = new HumanBuilder();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             Human human = humanBuilder.randomBuild();
-            System.out.println(human);
+            list.add(human);
         }
 
-        //        Animal animal1 = animalBuilder.setupType(AnimalType.BIRD)
-//                .setupEyeColor(EyeColor.BLACK)
-//                .setupWeight(123)
-//                .setupFur(false)
-//                .build();
-//        System.out.println(animal1);
+        System.out.println("---------");
+        System.out.println(list.size());
 
+        System.out.println("###################");
+        list.stream().forEach(System.out::println);
     }
 }
--- src/main/java/com/aston/tools/CustomArrayList.java
+++ src/main/java/com/aston/tools/CustomArrayList.java
@@ -4,61 +4,101 @@ package com.aston.tools;
 import java.util.AbstractList;
 import java.util.Arrays;
 
-public class CustomArrayList<E> extends AbstractList<E> {
-    private int size = 0;
-    private static final int DEFAULT_CAPACITY = 10;
-    private Object[] elements;
+    public class CustomArrayList<E> extends AbstractList<E> {
+        private int size = 0;
+        private static final int DEFAULT_CAPACITY = 10;
+        private Object[] elements;
 
-    public CustomArrayList() {
-        elements = new Object[DEFAULT_CAPACITY];
-    }
-
-    public CustomArrayList(int capacity) {
-        elements = new Object[capacity];
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public E get(int index) {
-        if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        public CustomArrayList() {
+            elements = new Object[DEFAULT_CAPACITY];
         }
-        return (E) elements[index];
-    }
 
-    @Override
-    public void add(int index, E element) {
-        if (index > size || index < 0) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        public CustomArrayList(int capacity) {
+            elements = new Object[capacity];
         }
-        ensureCapacity();
-        for (int i = size - 1; i >= index; i--) {
-            elements[i + 1] = elements[i];
-        }
-        elements[index] = element;
-        size++;
-    }
 
-    @Override
-    public E remove(int index) {
-        if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
-        }
-        Object item = elements[index];
-        for (int i = index; i < size - 1; i++) {
-            elements[i] = elements[i + 1];
-        }
-        size--;
-        return (E) item;
-    }
+        @Override
+        public int size() { return size; }
 
-    private void ensureCapacity() {
-        int newSize = elements.length * 2;
-        elements = Arrays.copyOf(elements, newSize);
-    }
+        @Override
+        public E get(int index) {
+            if (index >= size || index < 0) {
+                throw new IndexOutOfBoundsException("Index: " + index + ", Size " + size);
+            }
+            return (E) elements[index];
+        }
 
+        @Override
+        public void add(int index, E element) {
+            if (index > size || index < 0) {
+                throw new IndexOutOfBoundsException("Index: " + index + ", Size " + size);
+            }
+            ensureCapacity();
+            for (int i = size - 1; i >= index; i--) {
+                elements[i + 1] = elements[i];
+            }
+            elements[index] = element;
+            size++;
+        }
+
+        @Override
+        public E remove(int index) {
+            if (index >= size || index < 0) {
+                throw new IndexOutOfBoundsException("Index: " + index + ", Size " + size);
+            }
+            Object item = elements[index];
+            for (int i = index; i < size - 1; i++) {
+                elements[i] = elements[i + 1];
+            }
+            size--;
+            return (E) item;
+        }
+
+        // Ув.ёмкости массива
+        private void ensureCapacity() {
+            if (size == elements.length) {
+                int newSize = elements.length * 2;
+                elements = Arrays.copyOf(elements, newSize);
+            }
+        }
+
+        // Добавление метода для добавления элемента в конец списка, чтобы упростить использование класса
+        @Override
+        public boolean add(E element) {
+            ensureCapacity();
+            elements[size++] = element;
+            return true;
+        }
+
+        // Метод для очистки списка
+        @Override
+        public void clear() {
+            for (int i = 0; i < size; i++) {
+                elements[i] = null;
+            }
+            size = 0;
+        }
+
+        // поддержка итератора для форич цикла
+        @Override
+        public java.util.Iterator<E> iterator() {
+            return new java.util.Iterator<E>() {
+                private int currentIndex = 0;
+
+                @Override
+                public boolean hasNext() {
+                    return currentIndex < size;
+                }
+
+                @Override
+                public E next() {
+                    return get(currentIndex++);
+                }
+
+                @Override
+                public void remove() {
+                    throw new UnsupportedOperationException();
+                }
+            };
+        }
 }
